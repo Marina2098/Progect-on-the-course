84ab7e89a5792e27bb52898c41d590dd3660c3f9
Marina2098 <marina.pikalova.1998@gmail.com>
2023-03-27 23:23:59 +0200

    Initial commit

---
+++ file.js
@@ -0,0 +1,12 @@
+function validate() {
+   let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+   if (username == "" || password == "") {
+    alert("Please fill in all fields");
+   } else if (username == "user" && password == "password") {
+    alert("Login successful");
+   } else {
+    alert("Incorrect username or password");
+   }
+  }
